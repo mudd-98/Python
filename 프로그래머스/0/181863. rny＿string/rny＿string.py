a7dc8c347f5f6bac00d40eee39c7807c91f8687e
mudd-98 <kimboryoung98@gmail.com>
2025-09-29 17:10:02 +0900

    [level 0] Title: rny_string, Time: 0.01 ms, Memory: 9.15 MB -BaekjoonHub

---
+++ 프로그래머스/0/181863. rny＿string/rny＿string.py
@@ -0,0 +1,9 @@
+def solution(rny_string):
+    answer = ''
+    for s in rny_string:
+        if s == 'm':
+            answer += 'rn'
+        else:
+            answer += s
+            
+    return answer
