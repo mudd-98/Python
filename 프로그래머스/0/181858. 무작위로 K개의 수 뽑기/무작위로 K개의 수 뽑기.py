cfe21b7759c92b42b3fd38e9e6db6cc043273bb1
mudd-98 <kimboryoung98@gmail.com>
2025-10-01 16:36:13 +0900

    [level 0] Title: 무작위로 K개의 수 뽑기, Time: 63.89 ms, Memory: 9.57 MB -BaekjoonHub

---
+++ 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.py
@@ -0,0 +1,13 @@
+def solution(arr, k):
+    answer = []
+    
+    for a in arr:
+        if a not in answer:
+            answer.append(a)
+        if len(answer) == k:
+            break
+    
+    if len(answer) < k:
+        answer += [-1] * (k - len(answer))
+        
+    return answer
