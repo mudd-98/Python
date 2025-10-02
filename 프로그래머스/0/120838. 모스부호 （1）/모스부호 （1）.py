2a730ccef3372d850cd59a3f351deeeb5d957a0d
mudd-98 <kimboryoung98@gmail.com>
2025-10-02 17:39:37 +0900

    [level 0] Title: 모스부호 (1), Time: 0.11 ms, Memory: 9.16 MB -BaekjoonHub

---
+++ 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.py
@@ -0,0 +1,16 @@
+def solution(letter):
+    l_list = letter.split()
+    answer = ''
+    morse = { 
+    '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+    '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+    '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+    '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+    '-.--':'y','--..':'z'}
+    
+    for l in l_list:
+        for idx, (m,alpha) in enumerate(morse.items()):
+            if l == m:
+                answer += alpha
+    
+    return answer
