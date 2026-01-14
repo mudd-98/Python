3b86565e5cce62ff8a2017102445bd8c63311630
mudd-98 <kimboryoung98@gmail.com>
2026-01-14 14:22:12 +0900

    [Bronze II] Title: 방 배정, Time: 60 ms, Memory: 32412 KB, Score: 30 point -BaekjoonHub

---
+++ 백준/Bronze/13300. 방 배정/방 배정.py
@@ -0,0 +1,19 @@
+N, K = map(int, input().split())
+student = [0] * 12
+room = 0
+
+for _ in range(N):
+    S, Y = map(int, input().split())
+    
+    if(S == 0):
+        student[(Y * 2) - 1] += 1
+    else:
+        student[(Y * 2)] += 1
+
+for s in student:
+    if s == 0:
+        continue
+    # 나누어떨어지지 않을 때 자동으로 +1 해주는 식
+    room += (s + K - 1) // K
+
+print(room)
