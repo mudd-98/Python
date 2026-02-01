d36a14257c5fbadcb50b20e77fc65abb9ff00c07
mudd-98 <kimboryoung98@gmail.com>
2026-02-01 09:14:08 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+arr1 = [list(map(int, input().split())) for _ in range(n)]
+arr2 = [list(map(int, input().split())) for _ in range(n)]
+
+for i in range(n):
+  for j in range(m):
+    print(arr1[i][j] + arr2[i][j], end = ' ')
+  print()
